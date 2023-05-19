9c546aa06a357a26c36a6ebb29eb71b0dd7782ae
Edilin Valeri <edilinvaleri3@gmail.com>
2023-05-19 13:55:56 -0300

    1905

---
+++ src/app/app.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LOGOPRINCIPALComponent } from './components/logoprincipal/logoprincipal.component';
+import { FondoComponent } from './components/fondo/fondo.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FinComponent } from './components/fin/fin.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { HysComponent } from './components/hys/hys.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
+import { EditIntroComponent } from './components/intro/edit-intro.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LOGOPRINCIPALComponent,
+    FondoComponent,
+    IntroComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    FinComponent,
+    HomeComponent,
+    LoginComponent,
+    EditExperienciaComponent,
+    NewExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    HysComponent,
+    EditSkillComponent,
+    NewSkillComponent,
+    EditIntroComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage())
+  ],
+  
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendeavc',
+    appId: '1:969145269534:web:3c07ec87b13b33e0e55d0f',
+    storageBucket: 'frontendeavc.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC6kDvseraqrzrGsKVeAROUS7zoHakxnmw',
+    authDomain: 'frontendeavc.firebaseapp.com',
+    messagingSenderId: '969145269534',
+    measurementId: 'G-15WZWMY0PJ',
+  },
+  
+
+  production: true,
+    URL: 'https://edic.onrender.com/'
+  };
